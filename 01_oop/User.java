eb86778653fca6e87ed3a74cdbf593ec9610a956
Bidkar Aragón Cárdenas <bidkar.aragon@udo.mx>
2020-02-12 08:23:17 -0700

    OOP CRUD
    Se agregó la clase User y la base de datos en sqlite con tabla users

---
+++ 01_oop/User.java
@@ -0,0 +1,27 @@
+/**
+ * User
+ * 
+ * Id
+ * Name
+ * Password
+ * Email
+ * Firstname
+ * Lastname
+ * RememberToken
+ */
+public class User {
+    private int id;
+    private String name;
+    private String password;
+    private String email;
+    private String firstname;
+    private String lastname;
+    private String rememberToken;
+
+    // CRUD - Create Read Update Delete
+    public void Add() {}
+    public void All() {}
+    public void FindById(int id) {}
+    public void SaveChanges() {}
+    public void Delete(int id) {}
+}
